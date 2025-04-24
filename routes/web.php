43bbbfd82df9a88cbaaa5cd0213230e7681e593c
Aldren1948 <balindanaldren0@gmail.com>
2025-04-24 23:25:07 +0800

    issue--routing issue on contact page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('page/HomeLayout');
 });
+
+
+Route::get('contact', function () {
+    return view('page/ContactLayout');
+});
