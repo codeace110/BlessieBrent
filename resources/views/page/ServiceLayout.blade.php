cce591cdeda7f20c2eca8deeb05a311cd5a0ad69
Vhin2004 <jaysonpuyong3@gmail.com>
2025-04-25 21:39:07 -0700

    add service Layout!!

---
+++ resources/views/page/ServiceLayout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'My Laravel App')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="{{ asset('homepage/css/homepage.css') }}">
+
+    <!-- Page-specific styles -->
+    @yield('styles')
+</head>
+<body>
+
+    <!-- Navbar -->
+    @include('partials.HomeNav')
+
+    @include('partials.ServiceMain')
+
+    <!-- Footer -->
+    @include('partials.Homefooter')
+
+
+    <!-- Custom scripts -->
+    @yield('scripts')
+</body>
+</html>
